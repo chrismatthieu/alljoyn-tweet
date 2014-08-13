d403b6bfa10f1613410b226831bd40d55418f67e
Terry Bu <ktbucifer@gmail.com>
2014-08-13 16:39:05 -0400

    security measure by exporting out twitter keys to a module

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports.keys = {
+  consumer_key: 'BT8h5qcRYxa1opHZ4VX0DuW7h',
+  consumer_secret: 'xZul4Vk00ajT3wtUQrc8w2iPhP2vLRrmETaRtuk1iVGPrUvkau',
+  access_token_key: '161679280-PrYepOn61aW9udfHBampyQb5ZUzP9hKMSZAP6RI1',
+  access_token_secret: 'pksRxeQO1hb8i8aB5zqBBfb7Qg8RL87U1RFCyH0B1fbJd' };
--- twitter_server.js
+++ twitter_server.js
@@ -6,12 +6,11 @@ var port = "1337";
 var twitter = require('ntwitter');
 //writing the OAUTH authentication for accessing Twitter API is not necessary and people usually use this library to make things a lot easier (ntwitter)
 //Twitter Streaming API listens to all incoming tweets as they come in real-time
-var twit = new twitter({
-  consumer_key: 'BT8h5qcRYxa1opHZ4VX0DuW7h',
-  consumer_secret: 'xZul4Vk00ajT3wtUQrc8w2iPhP2vLRrmETaRtuk1iVGPrUvkau',
-  access_token_key: '161679280-PrYepOn61aW9udfHBampyQb5ZUzP9hKMSZAP6RI1',
-  access_token_secret: 'pksRxeQO1hb8i8aB5zqBBfb7Qg8RL87U1RFCyH0B1fbJd'
-});
+
+var myModule = require("./keys.js");
+
+var twit = new twitter(myModule.keys);
+//hiding my keys for security
 
 var app = express();
 app.get("/", function(request, response) {
