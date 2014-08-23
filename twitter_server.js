b07ce5cc5b120d69fdb32a4be101bebb314eecb4
Chris Matthieu <chris@matthieu.us>
2014-08-23 12:31:54 -0700

    initial commit

--- twitter_server.js
+++ twitter_server.js
@@ -1,50 +1,43 @@
-var express = require("express");
-var fs = require("fs");
-var http = require("http");
-var https = require("https");
-var port = "1337";
+var skynet = require("skynet");
 var twitter = require('ntwitter');
-//writing the OAUTH authentication for accessing Twitter API is not necessary and people usually use this library to make things a lot easier (ntwitter)
-//Twitter Streaming API listens to all incoming tweets as they come in real-time
-
 var twit = new twitter({
   consumer_key: 'BT8h5qcRYxa1opHZ4VX0DuW7h',
   consumer_secret: 'xZul4Vk00ajT3wtUQrc8w2iPhP2vLRrmETaRtuk1iVGPrUvkau',
   access_token_key: '161679280-PrYepOn61aW9udfHBampyQb5ZUzP9hKMSZAP6RI1',
-  access_token_secret: 'pksRxeQO1hb8i8aB5zqBBfb7Qg8RL87U1RFCyH0B1fbJd' });
-
-var app = express();
-app.get("/", function(request, response) {
-  var content = fs.readFileSync(__dirname + "/template.html");
-  response.setHeader("Content-Type", "text/html");
-  response.send(content);
-});
-var server = http.createServer(app);
-server.listen(port);
-
-var io = require('socket.io').listen(server);
-
-app.use(express.static(__dirname + '/public'));
-
-var count = 0;
-twit.stream('statuses/filter', { track: ['naruto'] }, function(stream) {
-  stream.on('data', function (tweet) {
-  	console.log("Passed a tweet to Socket IO");
-    count += 1;
-    console.log(count);
-  	io.sockets.emit("tweet", tweet);
-    // if (count >= 25) {
-    //   console.log("Stop twit stream since count >= 25");
-    //   stream.destroy();
-    // }
-  }); 
-  stream.on('end', function() {
-  	console.log("Disconnected");
-  	io.sockets.emit('ending', {message: 'OVERAGE'});
-  });
-  stream.on('error', function(error ,code) {
-    console.log("My error: " + error + ": " + code);
-  });
+  access_token_secret: 'pksRxeQO1hb8i8aB5zqBBfb7Qg8RL87U1RFCyH0B1fbJd' 
 });
 
+var skynet = require('skynet');
 
+var conn = skynet.createConnection({
+  // "uuid": "ad698900-2546-11e3-87fb-c560cb0ca47b",
+  // "token": "zh4p7as90pt1q0k98fzvwmc9rmjkyb9"
+});
+
+conn.on('ready', function(data){
+  console.log('UUID AUTHENTICATED!');
+  console.log(data);
+
+
+  twit.stream('statuses/filter', { track: ['allseenphx'] }, function(stream) {
+    stream.on('data', function (tweet) {
+    	console.log(tweet.text);
+      // Send and receive messages
+      conn.message({
+        "devices": "feadee3e-7cb5-4fb5-93bd-1bcdba8de1c5",
+        "subdevice":"alljoyn",
+        "payload": {
+          "method":"notify", "message": tweet.text
+        }
+      });
+
+    }); 
+    stream.on('end', function() {
+    	console.log("Disconnected");
+    });
+    stream.on('error', function(error ,code) {
+      console.log("My error: " + error + ": " + code);
+    });
+  });
+
+});
